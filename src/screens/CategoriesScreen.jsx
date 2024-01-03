e93c33b8e83937e97539260a1e41fa8df1216022
giuliano.lautaro <gigi2001.es@gmail.com>
2024-01-03 13:34:57 -0300

    delete message

--- src/screens/CategoriesScreen.jsx
+++ src/screens/CategoriesScreen.jsx
@@ -1,5 +1,4 @@
 import { FlatList, StyleSheet, Text, View,} from 'react-native'
-//import Header from '../components/Header'
 import categories_data from  '../data/categories_data.json'
 import CategoryItem from '../components/CategoryItem'
 
